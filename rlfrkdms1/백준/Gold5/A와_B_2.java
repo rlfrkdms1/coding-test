c2d0ee92b91e97bcddd931d62e0404c138d6cac0
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2025-02-26 00:49:26 +0900

    [250226] [Gold5] 12919번 A와 B 2
    Related to: #80

---
+++ rlfrkdms1/백준/Gold5/A와_B_2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String S = reader.readLine();
+        String T = reader.readLine();
+
+        int answer = 0;
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(T);
+
+        while(!queue.isEmpty() && answer == 0 && queue.peek().length() >= S.length()) {
+            String text = queue.poll();
+
+            if(text.length() == S.length()) {
+                if(text.equals(S)) answer = 1;
+                continue;
+            }
+
+            StringBuilder change = new StringBuilder(text);
+
+            if(change.charAt(change.length() - 1) == 'A') {
+                queue.offer(change.deleteCharAt(change.length() - 1).toString());
+                change.append('A');
+            }
+            
+            if(change.charAt(0) == 'B') queue.offer(change.deleteCharAt(0).reverse().toString());
+            
+        }
+        System.out.println(answer);
+    }
+
+}
