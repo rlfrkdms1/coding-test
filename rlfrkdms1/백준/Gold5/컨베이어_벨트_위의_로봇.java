1855606cb6dc449d9c90d12ed8814de9f7c2b93d
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2025-02-27 00:11:02 +0900

    [250227] [Gold5] 20055번 컨베이어 벨트 위의 로봇
    Related to: #81

---
+++ rlfrkdms1/백준/Gold5/컨베이어_벨트_위의_로봇.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    
+    static Deque<int[]> top = new ArrayDeque<>();
+    static Deque<int[]> bottom = new ArrayDeque<>();
+    static int n;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        int k = Integer.parseInt(tokenizer.nextToken());
+
+        tokenizer = new StringTokenizer(reader.readLine());
+
+        for(int i = 0; i < n; i++) {
+            top.offer(new int[] {Integer.parseInt(tokenizer.nextToken()), 0});
+        }
+
+        for(int i = 0; i < n; i++) {
+            bottom.offerFirst(new int[] {Integer.parseInt(tokenizer.nextToken()), 0});
+        }
+
+        int count0 = 0;
+        int step = 0;
+        while(count0 < k) {
+            step++;
+            
+            //1
+            bottom.offerLast(top.pollLast());
+            top.offerFirst(bottom.pollFirst());
+
+            getOffRobot();
+            
+            //2
+            for(int i = 0; i < n; i++) {
+                int[] target = top.pollLast();
+                int[] preTarget = top.peekFirst();
+                if(target[1] == 1 && preTarget[0] > 0 && preTarget[1] == 0) {
+                    target[1] = 0;
+                    preTarget[1] = 1;
+                    if(--preTarget[0] == 0) count0++;   
+                }
+                top.offerFirst(target);
+            }
+            getOffRobot();
+            
+            //3
+            int[] up = top.peekFirst();
+            if(up[0] > 0) {
+                if(--up[0] == 0) count0++;
+                up[1] = 1;
+            }
+            
+        }
+        System.out.println(step);
+    }
+
+    static void getOffRobot() {
+        int[] last = top.peekLast();
+        last[1] = 0;
+    }
+
+}
